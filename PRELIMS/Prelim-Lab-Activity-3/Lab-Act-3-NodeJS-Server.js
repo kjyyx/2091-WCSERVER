ef2a4c0c84e64e624ae53f68b7e4fe5f6622cb3a
kjyyx <122892187+kjyyx@users.noreply.github.com>
2023-01-30 12:19:53 +0800

    1 file modified

--- PRELIMS/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
+++ PRELIMS/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
@@ -6,8 +6,7 @@ var http = require('http'); // Import Node.js core module
 
 var server = http.createServer(function (req, res) {
   //create web server
-  if ((req.url = '/')) {
-    //check the URL of the current request
+  if ((req.url = '/')) { //check the URL of the current request
 
     // set response header
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -15,15 +14,22 @@ var server = http.createServer(function (req, res) {
     // set response content
     res.write('<html><body> <p>This is home Page.</p> </body></html>');
     res.end();
-  } else if ((req.url = '/student')) {
+
+  } 
+  
+  else if ((req.url = '/student')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body> <p>This is student Page.</p> </body></html>');
     res.end();
-  } else if ((req.url = '/student')) {
+  } 
+  
+  else if ((req.url = '/student')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body> <p>This is student Page.</p> </body></html>');
     res.end();
-  } else res.end('Invalid Request!');
+  } 
+  
+  else res.end('Invalid Request!');
 });
 
 server.listen(5000); //6 - listen for any incoming requests
