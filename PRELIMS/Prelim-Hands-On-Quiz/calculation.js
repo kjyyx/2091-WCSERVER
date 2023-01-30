64e3755a86e78895c300342c5ac6a8982cac525d
kjyyx <122892187+kjyyx@users.noreply.github.com>
2023-01-30 15:48:55 +0800

    2 files added

---
+++ PRELIMS/Prelim-Hands-On-Quiz/calculation.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(c, d) {
+  return c - d;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+module.exports = add, subtract, multiply, divide;
