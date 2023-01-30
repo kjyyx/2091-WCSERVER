5d99039cda382cf725cf91298a2efa09afe619f4
kjyyx <122892187+kjyyx@users.noreply.github.com>
2023-01-30 12:43:45 +0800

    1 file modified

--- PRELIMS/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
+++ PRELIMS/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
@@ -6,22 +6,28 @@ var http = require('http'); // Import Node.js core module
 
 var server = http.createServer(function (req, res) {
   //create web server
-  if ((req.url = '/')) { //check the URL of the current request
+  if ((req.url = '/root')) { //check the URL of the current request
 
     // set response header
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     // set response content
-    res.write('<html><body> <p>This is home Page.</p> </body></html>');
+    res.write('<html><body><h1>Welcome to my Node.js Application</h1><p>Welcome. This activity is about basics of Node.js</p> </body></html>');
     res.end();
   } 
   
-  else if ((req.url = '/student')) {
+  else if ((req.url = '/about')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body> <p>This is student Page.</p> </body></html>');
+    res.write('<html><body><h>This is the About Page</h1><p>Hello. This activity will teach you on how to deal with a simple server and local modules in Node.js</p></body></html>');
     res.end();
   } 
-  
+
+  else if ((req.url = '/contact')) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><h>This is the Contact Page</h1><p>if you wwant additional details about this activity go to this site: https</p></body></html>');
+    res.end();
+  } 
+
   else if ((req.url = '/admin')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body> <p>This is admin Page.</p> </body></html>');
