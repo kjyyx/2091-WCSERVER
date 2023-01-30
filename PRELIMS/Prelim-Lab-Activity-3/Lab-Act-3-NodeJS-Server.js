60541526f25d75b8d105e958d8c9397d0ca4e32c
kjyyx <122892187+kjyyx@users.noreply.github.com>
2023-01-30 12:02:14 +0800

    3 files added

---
+++ PRELIMS/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
@@ -0,0 +1,3 @@
+//Task Name: Laboratory Activity 3: Creating Node.js Application
+//Author: Kenji P. Jaculbia
+//Section: WD-201
---
+++ PRELIMS/Prelim-Lab-Demo-1/person/app.js
@@ -0,0 +1,3 @@
+var person = require('./Person.js');
+var person1 = new person ('James', 'Bond');
+console.log(person1.fullName());
