51a9234f0f7d0ed6ff4f3d786e73d82a154a05dc
kjyyx <122892187+kjyyx@users.noreply.github.com>
2023-01-30 12:55:14 +0800

    1 file modified

--- PRELIMS/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
+++ PRELIMS/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
@@ -12,25 +12,25 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     // set response content
-    res.write('<html><body><h1>Welcome to my Node.js Application</h1><p>Welcome. This activity is about basics of Node.js</p> </body></html>');
+    res.write('<html><body><h1>Welcome to my Node.js Application</h1><p>Welcome John Smith. This activity is about basics of Node.js</p> </body></html>');
     res.end();
   } 
   
   else if ((req.url == '/about')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><h>This is the About Page</h1><p>Hello. This activity will teach you on how to deal with a simple server and local modules in Node.js</p></body></html>');
+    res.write('<html><body><h>This is the About Page</h1><p>Hello John Smith. This activity will teach you on how to deal with a simple server and local modules in Node.js</p></body></html>');
     res.end();
   } 
 
   else if ((req.url == '/contact')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><h>This is the Contact Page</h1><p>if you wwant additional details about this activity go to this site: https://www.tutorialsteacher.com/nodejs</p></body></html>');
+    res.write('<html><body><h>This is the Contact Page</h1><p>John Smith, if you wwant additional details about this activity go to this site: https://www.tutorialsteacher.com/nodejs</p></body></html>');
     res.end();
   } 
 
-  else if ((req.url == '/admin')) {
+  else if ((req.url == '/gallery')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body> <p>This is admin Page.</p> </body></html>');
+    res.write('<html><body> <h1> This is the Gallery Page </h1> </body></html>');
     res.end();
   } 
   
