d9525aa1e8455893d8164c6e2d5d92d819bba69f
kjyyx <122892187+kjyyx@users.noreply.github.com>
2023-01-30 12:31:17 +0800

    1 file modified

--- PRELIMS/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
+++ PRELIMS/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
@@ -22,9 +22,9 @@ var server = http.createServer(function (req, res) {
     res.end();
   } 
   
-  else if ((req.url = '/student')) {
+  else if ((req.url = '/admin')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body> <p>This is student Page.</p> </body></html>');
+    res.write('<html><body> <p>This is admin Page.</p> </body></html>');
     res.end();
   } 
   
