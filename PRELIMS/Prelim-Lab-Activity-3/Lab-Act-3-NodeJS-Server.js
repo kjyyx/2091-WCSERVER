43885d29e091e34938a8bc43d3c152de35d7b218
kjyyx <122892187+kjyyx@users.noreply.github.com>
2023-01-30 13:02:13 +0800

    1 file modified

--- PRELIMS/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
+++ PRELIMS/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
@@ -12,19 +12,19 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     // set response content
-    res.write('<html><body><h1>Welcome to my Node.js Application</h1><p>Welcome John Smith. This activity is about basics of Node.js</p> </body></html>');
+    res.write('<html><body><h1>Welcome to my Node.js Application</h1><p>Welcome Kenji Jaculbia. This activity is about basics of Node.js</p> </body></html>');
     res.end();
   } 
   
   else if ((req.url == '/about')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><h>This is the About Page</h1><p>Hello John Smith. This activity will teach you on how to deal with a simple server and local modules in Node.js</p></body></html>');
+    res.write('<html><body><h>This is the About Page</h1><p>Hello Kenji Jaculbia This activity will teach you on how to deal with a simple server and local modules in Node.js</p></body></html>');
     res.end();
   } 
 
   else if ((req.url == '/contact')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><h1>This is the Contact Page</h1><p>John Smith, if you wwant additional details about this activity go to this site: https://www.tutorialsteacher.com/nodejs</p></body></html>');
+    res.write('<html><body><h1>This is the Contact Page</h1><p>Kenji Jaculbia, if you wwant additional details about this activity go to this site: https://www.tutorialsteacher.com/nodejs</p></body></html>');
     res.end();
   } 
 
