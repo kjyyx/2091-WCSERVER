8f1d2e41905468a3d8c40fec8342120c9e304d5d
kjyyx <122892187+kjyyx@users.noreply.github.com>
2023-02-08 15:57:52 +0800

    2 files modified

--- PRELIMS/PRELIM-EXAM/app.js
+++ PRELIMS/PRELIM-EXAM/app.js
@@ -2,7 +2,5 @@ var message = require('./message.js');
 var name = 'Kenji';
 
 console.log(
-  message.greet(name) +
-    message.congratulations('' + name) +
-    message.farewell('' + name)
+  message.greet(name) + message.congratulations(name) + message.farewell(name)
 );
--- PRELIMS/PRELIM-EXAM/message.js
+++ PRELIMS/PRELIM-EXAM/message.js
@@ -1,11 +1,11 @@
 function greet(name) {
-  return 'Hello, ' + name + '!';
+  return 'Hello, ' + name + '!,';
 }
 function congratulations(name) {
-  return 'Congratulations, ' + name + '!';
+  return ' Congratulations, ' + name + '!,';
 }
 function farewell(name) {
-  return 'Goodbye, ' + name + '!';
+  return ' Goodbye, ' + name + '!';
 }
 
 module.exports = { greet, congratulations, farewell };
